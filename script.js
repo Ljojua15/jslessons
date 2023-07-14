1968952c7a8563c60659803417dd8023d7dd8a93
Ljojua15 <97795530+Ljojua15@users.noreply.github.com>
2023-07-14 16:29:26 +0400

    new lessons

---
+++ script.js
@@ -0,0 +1,30 @@
+// let rudd = [`pidor`, `gay`, `boss`, `boosi`];
+
+// rudd.forEach((saxeli, index) => {
+//   console.log(`sandros saxeli aris - ${saxeli}, index aris - ${index}`);
+// });
+
+// let las = (x) => {
+//   if (x > 10) {
+//     console.log(`${x} მეტი არის 10-ზე`);
+//   } else if (x == 10) {
+//     console.log(`${x} უდრის 10`);
+//   } else {
+//     console.log(`${x} ნაკლებია 10-ზე`);
+//   }
+// };
+
+// las(10);
+
+let x = true;
+
+switch (x) {
+  case Number:
+    console.log("gatishva");
+    break;
+  case Boolean:
+    console.log("chartva");
+    break;
+  default:
+    console.log("mnishvneloba ver");
+}
